c002e8238b632d17053663dfd47eac37f9b50138
Glitch (glitch-hello-node) <none>
2025-05-30 14:23:24 +0000

    🗺🕍 Checkpoint ./server.js:27790248/4601

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const { Server } = require('socket.io');
 
-/** 
- * Class to track each connected player (id + position)
- */
 class Player {
     constructor(id) {
         this.id = id;
@@ -15,78 +12,63 @@ class Player {
 
 const server = http.createServer();
 
-// Configure Socket.IO with CORS
 const io = new Server(server, {
     cors: {
-        // If you only want to allow PlayCanvas launch domain:
-        // origin: "https://launch.playcanvas.com",
-
-        // Or allow all origins (less secure, but quick for testing)
         origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
 const players = {};
-this.sphereOwner=""
-/**
- * Handle new socket connections
- */
+let sphereOwner = null;
+
 io.on('connection', (socket) => {
     console.log(`New client connected: ${socket.id}`);
 
-    // Fired when the client is ready to initialize their Player object
     socket.on('initialize', () => {
         const newPlayer = new Player(socket.id);
         players[socket.id] = newPlayer;
 
-        // Send to this client its own ID and the current list of players
-        socket.emit('playerData', { id: socket.id, players });
-
-        // Tell everyone else about this new player
+        socket.emit('playerData', { id: socket.id, players, owner: sphereOwner });
         socket.broadcast.emit('playerJoined', newPlayer);
     });
-  socket.on('claimSphere', (data) => {
-    
-    console.log(`${data.id} claimed sphere ownership`);
-    this.sphereOwner = data.id;  // keep track of current owner
 
-    // Notify all clients about new owner
-    
-});
+    socket.on('claimSphere', (data) => {
+        console.log(`${data.id} claimed sphere ownership`);
+        sphereOwner = data.id;
+        io.emit('sphereOwnershipChanged', { owner: sphereOwner });
+    });
 
-  
-  
-socket.on('spherePositionUpdate', (data) => {
-  console.log(data.id,this.sphereOwner)
-  if (data.id==this.sphereOwner)
-   {console.log("hey",data)
-     socket.broadcast.emit('updateSphere', data);}
-});
-    // Update player position
+    socket.on('spherePositionUpdate', (data) => {
+        if (data.id === sphereOwner) {
+            io.emit('updateSphere', data);
+        }
+    });
+
     socket.on('positionUpdate', (data) => {
         if (!players[socket.id]) return;
         players[socket.id].x = data.x;
         players[socket.id].y = data.y;
         players[socket.id].z = data.z;
-//console.log(data)
-        // Broadcast updated position to all other players
+
         socket.broadcast.emit('playerMoved', {
             id: socket.id,
             x: data.x,
             y: data.y,
             z: data.z,
-          face:data.face
+            face: data.face
         });
     });
 
-    // Handle disconnections
     socket.on('disconnect', () => {
         console.log(`Client disconnected: ${socket.id}`);
-        if (!players[socket.id]) return;
         delete players[socket.id];
-        // Notify other players to remove this player
         socket.broadcast.emit('killPlayer', socket.id);
+
+        if (sphereOwner === socket.id) {
+            sphereOwner = null;
+            io.emit('sphereOwnershipChanged', { owner: null });
+        }
     });
 });
 
